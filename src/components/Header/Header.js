f2acb931f5c31bb003ac7a88417da7b9991c9f19
unknown <sutherlandscott21@gmail.com>
2019-10-11 16:59:30 -0600

    added buttons in header in prep for routing

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import './Header.css'
 
 export default class Header extends React.Component {
     render(){
         return( 
-            <div>
-                Header
+            <div className='header'>
+                
+                <h1>SHELFIE</h1>
+                <button>Dashboard</button>
+                <button>Add Inventory</button>
+
             </div>
         )
     }
